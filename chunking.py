e6048ac3975876a68d22dce0d7825467298b1514
Harsh Kumar <badmitonlover@gmail.com>
2026-05-23 11:12:04 +0530

    Fixed the import error on line 1

--- chunking.py
+++ chunking.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def chunk_texts(pages: list[str], chunk_size=512, chunk_overlap=64):
     splitter = RecursiveCharacterTextSplitter(
